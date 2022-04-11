282943a9f5351c5a580e1392628d1fe4c9f36f17
ImScarecrow5 <vriilux@yandex.ru>
2022-04-11 12:39:51 +0300

    Попытки достать картинку

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from PIL import Image, ImageDraw
 
@@ -13,8 +13,6 @@ logger = logging.getLogger(__name__)
 TOKEN = "5294422299:AAG8VQO8dV3thi-DE1UCNREFrI82NCVy3qY"
 
 # Создадим стартовую клавиатуру
-reply_keyboard = ["/help"]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
 
 # Функция удаления клавиатуры
@@ -24,32 +22,37 @@ def close_keyboard(update, context):
 
 # Функция приветствия пользователя
 def start(update, context):
-    global markup
-    update.message.reply_text("Привет, я фотошоп бот. Для ознакомления с моим функционалом напиши команду /help",
-                              reply_markup=markup)
+    update.message.reply_text("Привет, я фотошоп бот. Для ознакомления с моим функционалом напиши команду /help")
 
 
 def help(update, context):
     update.message.reply_text("Пока что я балбес")
 
+    # @bot.message_handler(content_types=['photo', 'document'])
+# def download_image(message):
+    # from pathlib import Path
+    # Path(f'files/{message.chat.id}/').mkdir(parents=True, exist_ok=True)
+    # if message.content_type == 'photo':
+        # file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
+        # downloaded_file = bot.download_file(file_info.file_path)
+        # src = f'files/{message.chat.id}/' + file_info.file_path.replace('photos/', '')
+        # with open(src, 'wb') as new_file:
+            # new_file.write(downloaded_file)
 
-@bot.message_handler(content_types=['photo', 'document'])
-def download_image(message):
-    from pathlib import Path
-    Path(f'files/{message.chat.id}/').mkdir(parents=True, exist_ok=True)
-    if message.content_type == 'photo':
-        file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-        src = f'files/{message.chat.id}/' + file_info.file_path.replace('photos/', '')
-        with open(src, 'wb') as new_file:
-            new_file.write(downloaded_file)
 
-    elif message.content_type == 'document':
-        file_info = bot.get_file(message.document.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-        src = f'files/{message.chat.id}/' + message.document.file_name
-        with open(src, 'wb') as new_file:
-            new_file.write(downloaded_file)
+    # elif message.content_type == 'document':
+        # file_info = bot.get_file(message.document.file_id)
+        # downloaded_file = bot.download_file(file_info.file_path)
+        # src = f'files/{message.chat.id}/' + message.document.file_name
+        # with open(src, 'wb') as new_file:
+            # new_file.write(downloaded_file)
+
+
+def get_image(update, context):
+    image = update.message.photo[-1]
+    src = "C/" + update.message.file_name
+    with open(src, "wb") as new_file:
+        new_file.write(image)
 
 
 def turn_right():
@@ -78,31 +81,37 @@ def getting_the_3D_effect():
 
 # Основная функция
 def main():
-    global markup
     # Создаем объект updater
     updater = Updater(TOKEN)
 
     # Получаем из updater'а диспетчер сообщений
     dp = updater.dispatcher
 
-    # Зарегистрируем 2 базовых команды, команду закрытия клавиатуры и
-    # основные команды для работы с картинками
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("reply_image", download_image))
-    # dp.add_handler(CommandHandler("close", close_keyboard))
-    # dp.add_handler(CommandHandler("turn_right", turn_right))
-    # dp.add_handler(CommandHandler("turn_left", turn_left))
-    # dp.add_handler(CommandHandler("flip_vertical", flip_vertical))
-    # dp.add_handler(CommandHandler("flip_horizontally", flip_horizontally))
-    # dp.add_handler(CommandHandler("compression", compression))
-    # dp.add_handler(CommandHandler("3D_effect", getting_the_3D_effect))
+    # Создаем обработчики
+    start_handler = CommandHandler("start", start)
+    help_handler = CommandHandler("help", help)
+    image_handler = MessageHandler(Filters.document.image & (~Filters.command), get_image)
+    close_keyboard_handler = CommandHandler("close", close_keyboard)
+    turn_right_handler = CommandHandler("turn_right", turn_right)
+    turn_left_handler = CommandHandler("turn_left", turn_left)
+    flip_vertical_handler = CommandHandler("flip_vertical", flip_vertical)
+    flip_horizontally_handler = CommandHandler("flip_horizontally", flip_horizontally)
+    compression_handler = CommandHandler("compression", compression)
+    effect_handler = CommandHandler("3D_effect", getting_the_3D_effect)
+
+    # Регистрируем обработчики
+    dp.add_handler(start_handler)
+    dp.add_handler(help_handler)
+    dp.add_handler(image_handler)
+    dp.add_handler(close_keyboard_handler)
+    dp.add_handler(turn_right_handler)
+    dp.add_handler(turn_left_handler)
+    dp.add_handler(flip_vertical_handler)
+    dp.add_handler(flip_horizontally_handler)
+    dp.add_handler(compression_handler)
+    dp.add_handler(effect_handler)
 
     # Создадим клавиатуру для выбора одной из команд
-    reply_keyboard = [["/turn_right", "/turn_left"],
-                      ["/flip_vertical", "/flip_horizontally"],
-                      ["/compression", "/3D_effect"]]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
     # Запускаем цикл приема и обработки сообщений
     updater.start_polling()
