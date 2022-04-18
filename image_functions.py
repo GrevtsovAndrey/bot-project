c23178e32fdb0a949340bf34fc39875100e9e640
Andrei <grevtsov.andrey@bk.ru>
2022-04-18 09:48:15 +0300

    Вынес функции для работы с изображением в отдельный файл, добавил комментарии

---
+++ image_functions.py
@@ -0,0 +1,88 @@
+from PIL import Image
+
+
+# Поворот вправо
+def turn_right(update, context):
+    im = Image.open("telegram_image.jpg")
+    im.transpose(Image.ROTATE_270).save('telegram_image.jpg')
+    id = update.message.chat.id
+    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
+
+
+# Поворот влево
+def turn_left(update, context):
+    im = Image.open("telegram_image.jpg")
+    im.transpose(Image.ROTATE_90).save('telegram_image.jpg')
+    id = update.message.chat.id
+    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
+
+
+# Отражение по вертикали
+def flip_vertical(update, context):
+    im = Image.open("telegram_image.jpg")
+    pix_s = im.load()
+    x, y = im.size
+    for i in range(x // 2):
+        for j in range(y):
+            pix_s[i, j], pix_s[x - i - 1, j] = pix_s[x - i - 1, j], pix_s[i, j]
+    im.save("telegram_image.jpg")
+    id = update.message.chat.id
+    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
+
+
+# Отражение по горизонтали
+def flip_horizontally(update, context):
+    im = Image.open("telegram_image.jpg")
+    pix_s = im.load()
+    x, y = im.size
+    for i in range(x // 2):
+        for j in range(y):
+            pix_s[i, j], pix_s[x - i - 1, j] = pix_s[x - i - 1, j], pix_s[i, j]
+    im.transpose(Image.ROTATE_180).save('telegram_image.jpg')
+    id = update.message.chat.id
+    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
+
+
+# Черно-белые эффект
+def white_black(update, context):
+    im = Image.open("telegram_image.jpg")
+    pix_s = im.load()
+    x, y = im.size
+    for i in range(x):
+        for j in range(y):
+            r, g, b = pix_s[i, j]
+            bw = (r + g + b) // 3
+            pix_s[i, j] = bw, bw, bw
+    im.save("telegram_image.jpg")
+    id = update.message.chat.id
+    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
+
+
+# 3D еффект
+def getting_the_3D_effect(update, context):
+    delta = 8
+    im = Image.open("telegram_image.jpg")
+    x, y = im.size
+    imR = im.copy()
+    pixR = imR.load()
+    for i in range(x):
+        for j in range(y):
+            r = list(pixR[i, j])[0]
+            pixR[i, j] = (r, 0, 0)
+    imGB = im.copy()
+    pixGB = imGB.load()
+    for i in range(x):
+        for j in range(y):
+            g = list(pixGB[i, j])[1]
+            b = list(pixGB[i, j])[2]
+            pixGB[i, j] = (0, g, b)
+    for i in range(x):
+        for j in range(y):
+            if i - delta >= 0:
+                r, g, b = pixGB[i, j]
+                R = list(pixR[i - delta, j])[0]
+                pixGB[i, j] = (r + R, g, b)
+    im = imGB.copy()
+    im.save("telegram_image.jpg")
+    id = update.message.chat.id
+    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import image_functions
+
 import logging
 
 from telegram import ReplyKeyboardRemove, ReplyKeyboardMarkup
@@ -5,7 +7,6 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import requests
 
-from PIL import Image
 
 # Запускаем логгирование
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
@@ -69,87 +70,6 @@ def new_image(update, context):
                               reply_markup=markup)
 
 
-def turn_right(update, context):
-    im = Image.open("telegram_image.jpg")
-    im.transpose(Image.ROTATE_270).save('telegram_image.jpg')
-    id = update.message.chat.id
-    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
-
-
-def turn_left(update, context):
-    im = Image.open("telegram_image.jpg")
-    im.transpose(Image.ROTATE_90).save('telegram_image.jpg')
-    id = update.message.chat.id
-    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
-
-
-def flip_vertical(update, context):
-    im = Image.open("telegram_image.jpg")
-    pix_s = im.load()
-    x, y = im.size
-    for i in range(x // 2):
-        for j in range(y):
-            pix_s[i, j], pix_s[x - i - 1, j] = pix_s[x - i - 1, j], pix_s[i, j]
-    im.save("telegram_image.jpg")
-    id = update.message.chat.id
-    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
-
-
-def flip_horizontally(update, context):
-    im = Image.open("telegram_image.jpg")
-    pix_s = im.load()
-    x, y = im.size
-    for i in range(x // 2):
-        for j in range(y):
-            pix_s[i, j], pix_s[x - i - 1, j] = pix_s[x - i - 1, j], pix_s[i, j]
-    im.transpose(Image.ROTATE_180).save('telegram_image.jpg')
-    id = update.message.chat.id
-    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
-
-
-def white_black(update, context):
-    im = Image.open("telegram_image.jpg")
-    pix_s = im.load()
-    x, y = im.size
-    for i in range(x):
-        for j in range(y):
-            r, g, b = pix_s[i, j]
-            bw = (r + g + b) // 3
-            pix_s[i, j] = bw, bw, bw
-    im.save("telegram_image.jpg")
-    id = update.message.chat.id
-    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
-
-
-def getting_the_3D_effect(update, context):
-    delta = 8
-    im = Image.open("telegram_image.jpg")
-    x, y = im.size
-    imR = im.copy()
-    pixR = imR.load()
-    for i in range(x):
-        for j in range(y):
-            r = list(pixR[i, j])[0]
-            pixR[i, j] = (r, 0, 0)
-    imGB = im.copy()
-    pixGB = imGB.load()
-    for i in range(x):
-        for j in range(y):
-            g = list(pixGB[i, j])[1]
-            b = list(pixGB[i, j])[2]
-            pixGB[i, j] = (0, g, b)
-    for i in range(x):
-        for j in range(y):
-            if i - delta >= 0:
-                r, g, b = pixGB[i, j]
-                R = list(pixR[i - delta, j])[0]
-                pixGB[i, j] = (r + R, g, b)
-    im = imGB.copy()
-    im.save("telegram_image.jpg")
-    id = update.message.chat.id
-    context.bot.send_photo(chat_id=id, photo=open("telegram_image.jpg", "rb"))
-
-
 # Основная функция
 def main():
     # Создаем объект updater
@@ -163,12 +83,12 @@ def main():
     help_handler = CommandHandler("help", help)
     image_handler = MessageHandler(Filters.photo, new_image)
     close_keyboard_handler = CommandHandler("close", close_keyboard)
-    turn_right_handler = CommandHandler("turn_right", turn_right)
-    turn_left_handler = CommandHandler("turn_left", turn_left)
-    flip_vertical_handler = CommandHandler("flip_vertical", flip_vertical)
-    flip_horizontally_handler = CommandHandler("flip_horizontally", flip_horizontally)
-    white_black_handler = CommandHandler("white_black", white_black)
-    effect_handler = CommandHandler("3D_effect", getting_the_3D_effect)
+    turn_right_handler = CommandHandler("turn_right", image_functions.turn_right)
+    turn_left_handler = CommandHandler("turn_left", image_functions.turn_left)
+    flip_vertical_handler = CommandHandler("flip_vertical", image_functions.flip_vertical)
+    flip_horizontally_handler = CommandHandler("flip_horizontally", image_functions.flip_horizontally)
+    white_black_handler = CommandHandler("white_black", image_functions.white_black)
+    effect_handler = CommandHandler("3D_effect", image_functions.getting_the_3D_effect)
 
     # Регистрируем обработчики
     dp.add_handler(start_handler)
